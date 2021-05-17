0fd1d0b3a071a905b7d57fcd54601e49fbf7be57
LaloDavilaC9 <eduardo.davilac9@gmail.com>
2021-05-17 18:36:32 -0500

    xasdas

--- proyectopruebaImagenes/src/proyectopruebaimagenes/Imagenes.java
+++ proyectopruebaImagenes/src/proyectopruebaimagenes/Imagenes.java
@@ -37,7 +37,7 @@ public class Imagenes extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel3.setIcon(new ImageIcon("src/imagenes/maincra.jpg"));
+        jLabel3.setIcon(new javax.swing.ImageIcon("D:\\chido\\Documents\\Tareas\\UNIVERSIDAD\\Cuarto Semestre\\Programación III\\ProyectoFinal\\Nueva carpeta\\PruebaCintia\\PruebaCintia\\proyectopruebaImagenes\\src\\imagenes\\maincra.jpg")); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -94,6 +94,7 @@ public class Imagenes extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Imagenes().setVisible(true);  
+              
             }
         });
     }
